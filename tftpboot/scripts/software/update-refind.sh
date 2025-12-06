8d63d6eda9c5f3b47c40243df09333955ea8d4e0
MAHDTech <MAHDTech@saltlabs.tech>
2025-12-06 15:59:20 +1100

    fix: update refind

--- tftpboot/scripts/software/update-refind.sh
+++ tftpboot/scripts/software/update-refind.sh
@@ -12,13 +12,13 @@ IMG_LOCAL="$(pwd)/tftpboot/iso/refind.img"
 export IMG_REMOTE="bootycall:/mnt/hdd/tftpboot/iso/refind.img"
 export IMG_SIZE="64"
 
-export REFIND_VER="0.14.2"
+export REFIND_VER="0.14.1"
 export REFIND_BIN="refind_extracted/refind-bin-${REFIND_VER}"
 export REFIND_THEME_ENABLED="true"
 export REFIND_THEME_REPO="https://github.com/ussooraj/rEFInd-Synthwave.git"
 export REFIND_THEME_DIR="refind-custom-theme"
 
-export EFI_BOOT="EFI/BOOT"
+export EFI_DIR="EFI"
 
 mkdir -p "$WORK_DIR" || {
 	echo "Failed to create a temporary working directory"
@@ -35,9 +35,7 @@ cat <<-_EOF_ >"${WORK_DIR}/run.sh"
 		popd 2>/dev/null
 
 		if [ "${DEBUG:-false}" == "true" ]; then
-			echo "Debug mode enabled, dumping information..."
-			tree "${WORK_DIR}" || true
-			cat "${WORK_DIR}/run.sh" || true
+			read -rp "Press Enter to begin cleanup operation on rEFInd working directory: ${WORK_DIR}..." RESPONSE
 		fi
 
 		rm -rf "${WORK_DIR}" || {
@@ -79,35 +77,64 @@ cat <<-_EOF_ >"${WORK_DIR}/run.sh"
 			exit 1
 		}
 
-	show_header "Creating directory structure"
+	show_header "Creating directory structure for EFI"
 
-	mkdir -p "${EFI_BOOT}" || {
-		echo "Failed to create EFI boot directory"
+	mkdir -p "${EFI_DIR}/BOOT" || {
+		echo "Failed to create directories for EFI"
 		exit 1
 	}
 
-	cp "${REFIND_BIN}/refind/refind_x64.efi" "${EFI_BOOT}/BOOTX64.EFI" || {
+	cp "${REFIND_BIN}/refind/refind_x64.efi" "${EFI_DIR}/BOOT/BOOTX64.EFI" || {
 		echo "Failed to copy rEFInd binary"
 		exit 1
 	}
 
-	mkdir -p "${EFI_BOOT}/drivers" || {
-		echo "Failed to create drivers directory"
+	show_header "Creating directory structure for rEFInd common folders"
+
+	REFIND_FOLDERS=(
+		"assets"
+		"fonts"
+		"icons"
+		"images"
+		"keys"
+	)
+
+	for FOLDER in ${REFIND_FOLDERS[@]}; do
+		echo "Processing rEFInd folder: ${FOLDER}"
+		mkdir -p "${EFI_DIR}/refind/${FOLDER}" || {
+			echo "Failed to create directory ${FOLDER} for rEFInd"
+			exit 1
+		}
+		cp -r "${REFIND_BIN}/refind/${FOLDER}/"* "${EFI_DIR}/refind/${FOLDER}/" || {
+			echo "Failed to copy rEFInd ${FOLDER}"
+			exit 1
+		}
+	done
+
+	show_header "Creating directory structure for rEFInd architecture folders"
+
+	mkdir -p "${EFI_DIR}/refind/"{drivers,tools} || {
+		echo "Failed to create directories for rEFInd"
 		exit 1
 	}
 
-	cp "${REFIND_BIN}/refind/drivers_x64/"* "${EFI_BOOT}/drivers/" || {
+	cp "${REFIND_BIN}/refind/drivers_x64/"* "${EFI_DIR}/refind/drivers/" || {
 		echo "Failed to copy rEFInd drivers"
 		exit 1
 	}
 
-	mkdir -p "${EFI_BOOT}/icons" || {
-		echo "Failed to create icons directory"
+	cp "${REFIND_BIN}/refind/tools_x64/"* "${EFI_DIR}/refind/tools/" || {
+		echo "Failed to copy rEFInd tools"
 		exit 1
 	}
 
 	if [ "${REFIND_THEME_ENABLED}" = "true" ]; then
-		echo "Using custom rEFInd theme"
+		show_header "Creating directory structure for rEFInd custom theme"
+
+		mkdir -p "${EFI_DIR}/refind/theme/"{assets,icons} || {
+			echo "Failed to create directory for rEFInd custom theme"
+			exit 1
+		}
 
 		git clone "${REFIND_THEME_REPO}" "${REFIND_THEME_DIR}" || {
 			echo "Failed to clone rEFInd custom theme into ${REFIND_THEME_DIR}"
@@ -116,7 +143,7 @@ cat <<-_EOF_ >"${WORK_DIR}/run.sh"
 
 		if [ -d "${REFIND_THEME_DIR}/icons" ]; then
 			echo "Copying custom rEFInd theme icons"
-			cp -r "${REFIND_THEME_DIR}/icons" "${EFI_BOOT}/icons/" || {
+			cp -r "${REFIND_THEME_DIR}/icons"/* "${EFI_DIR}/refind/theme/icons/" || {
 				echo "Failed to copy custom rEFInd theme icons"
 				exit 1
 			}
@@ -127,7 +154,7 @@ cat <<-_EOF_ >"${WORK_DIR}/run.sh"
 
 		if [ -d "${REFIND_THEME_DIR}/assets" ]; then
 			echo "Copying custom rEFInd theme assets"
-			cp -r "${REFIND_THEME_DIR}/assets" "${EFI_BOOT}/icons/" || {
+			cp -r "${REFIND_THEME_DIR}/assets"/* "${EFI_DIR}/refind/theme/assets/" || {
 				echo "Failed to copy custom rEFInd theme assets"
 				exit 1
 			}
@@ -138,19 +165,16 @@ cat <<-_EOF_ >"${WORK_DIR}/run.sh"
 
 	else
 		echo "Using default rEFInd theme"
-		cp -r "${REFIND_BIN}/refind/icons/"* "${EFI_BOOT}/icons/" || {
-			echo "Failed to copy rEFInd icons"
-			exit 1
-		}
 	fi
 
 	show_header "Creating rEFInd Config"
 
 	# Common configuration
-	cat <<-EOF > "${EFI_BOOT}/refind.conf"
+	cat <<-EOF > "${EFI_DIR}/refind/refind.conf"
 	# Settings
 	timeout                   10
 	screensaver               30
+	log_level                 3
 	use_graphics_for          osx,linux,elilo,grub,windows
 	showtools                 firmware,reboot,shutdown
 	hideui                    singleuser,arrows,hints,badges,label
@@ -158,22 +182,26 @@ cat <<-_EOF_ >"${WORK_DIR}/run.sh"
 	scanfor                   internal,external,optical,biosexternal
 
 	EOF
-
 	# Theme configuration
 	if "${REFIND_THEME_ENABLED:-false}" == "true"; then
 		echo "Using custom rEFInd theme"
-		cat <<-EOF >> "${EFI_BOOT}/refind.conf"
+		cat <<-EOF >> "${EFI_DIR}/refind/refind.conf"
 		# Resources
-		icons_dir                 icons
-		banner                    assets/background.png
-		selection_big             assets/selection_big_v3.png
-		selection_small           assets/selection_small_v3.png
+		icons_dir                 /EFI/refind/theme/icons
+		banner                    /EFI/refind/theme/assets/background.png
+		selection_big             /EFI/refind/theme/assets/selection_big_v3.png
+		selection_small           /EFI/refind/theme/assets/selection_small_v3.png
 
 		# Scale
 		banner_scale              fillscreen
 		big_icon_size             125
 		small_icon_size           48
 
+		EOF
+	else
+		echo "Using default rEFInd theme"
+		cat <<-EOF >> "${EFI_DIR}/refind/refind.conf"
+		# TODO: Add default theme configuration here.
 		EOF
 	fi
 
@@ -222,6 +250,8 @@ nix-shell -p \
 	git \
 	mtools \
 	openssh \
+	refind \
+	rsync \
 	tree \
 	unzip \
 	--pure \
